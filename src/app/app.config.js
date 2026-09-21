8e2879be53bce334c1a0fa5c75fcb2893c7a1da7
Critian Choc <cristianchoc001@gmail.com>
2026-09-20 22:09:07 -0600

    Add files via upload

---
+++ src/app/app.config.js
@@ -0,0 +1,10 @@
+import { provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+export const appConfig = {
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(routes), provideClientHydration()
+    ]
+};
